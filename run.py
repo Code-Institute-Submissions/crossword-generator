c53d4f8309801553df6e96cad3548ddb552f317b
John Rearden <isnocturnal@gmail.com>
2022-07-23 09:10:50 +0100

    Add find_matches function to run.py

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ class Crossword:
         self.print()
 
     def print(self):
+        """Print the crossword to the terminal"""
         light_gray = Colors.get_background_color(220, 220, 220)
         dark_gray = Colors.get_background_color(0, 0, 0)
         text_color = Colors.get_foreground_color(0, 0, 0)
@@ -35,6 +36,38 @@ class Crossword:
 def main():
     """Main entry point for the program"""
     crossword = Crossword(11, 11)
+    word_dict = {}
+    with open('data/large_dict_words_only.txt', 'r') as file:
+        for word in file:
+            word = word.replace('\n', '')
+            length = len(word)
+            if length in word_dict.keys():
+                word_dict[length].append(word)
+            else:
+                word_dict[length] = []
+                word_dict[length].append(word)
+    for key, value in word_dict.items():
+        print(f"Number of {key}-letter words is {len(value)}")
+    print(word_dict[6][0])
+    find_matches("anti_____", word_dict)
+
+def find_matches(word, word_dict):
+    """Searches the word_dict to find matches for the supplied word"""
+    known_chars = []
+    for i, char in enumerate(word):
+        if char != '_':
+            known_chars.append((i, char))
+    potential_matches = word_dict[len(word)]
+    matches = []
+    for potential_match in potential_matches:
+        match = True
+        for char_tuple in known_chars:
+            index, char = char_tuple
+            if potential_match[index] != char:
+                match = False
+        if match:
+            matches.append(potential_match)
+    return matches
 
 
 if __name__ == '__main__':
