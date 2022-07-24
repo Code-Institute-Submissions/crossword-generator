2632d8c9b5b2514d77033a827568d17f1ca901a6
John Rearden <isnocturnal@gmail.com>
2022-07-24 09:18:25 +0100

    Add function to check if a cell in a potential new word is legal or not

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+from grp import getgrgid
 import random
+from tkinter import HORIZONTAL
 from constants import Orientation, get_large_letter, Colors, UniChars, AnsiCommands
 
 class Word:
@@ -13,7 +15,7 @@ class Crossword:
     def __init__(self, cols, rows, word_dict):
         self.cols = cols
         self.rows = rows
-        self.grid = [["_" for i in range(cols)] for j in range(rows)]
+        self.grid = [["_" for i in range(rows)] for j in range(cols)]
         self.word_dict = word_dict
         self.print()
         self.generate_words()
@@ -26,19 +28,66 @@ class Crossword:
         blank_string = ''.join(blank_chars)
         print(f"{blank_string} (length = {len(blank_string)})")
         first_word = Word(Orientation.HORIZONTAL, blank_string, 0, 0)
+        start_positions = [(0, 2), (0, 4), (0, 6), (0, 8), (0, 10)]
         matches = find_matches(first_word.string, self.word_dict)
         choice = random.choice(matches)
-        print(choice)
         self.add_word_to_grid(Word(Orientation.HORIZONTAL, choice, 0, 0))
+
+    def _check_cell_is_legal(self, row, col, orientation):
+        """Checks if the cell can be used as part of a new word in the crossword"""
+
+        # Only check neighbours if this cell is blank - adding a letter at the start
+        # or end of an existing word should be avoided, as it may not result in a new legal
+        # word. Running a new word through an existing word is fine, as it does not alter
+        # the existing word. In fact, it's the whole point of a crossword.
+        if self.grid(row, col) != "_":
+            return True
+
+        if orientation == Orientation.HORIZONTAL:
+            has_cell_to_left = col > 0
+            has_cell_to_right = col < self.cols
+            if has_cell_to_left and self.grid[row][col - 1] != "_":
+                return False
+            if has_cell_to_right and self.grid[row][col + 1] != "_":
+                return False
+        elif orientation == Orientation.VERTICAL:
+            has_cell_above = row > 0
+            has_cell_below = row < self.rows
+            if has_cell_above and self.grid[row -1][col] != "_":
+                return False
+            if has_cell_below and self.grid[row + 1][col] != "_":
+                return False
+        
+        return True
+
+    def _vertical_neighbour_check(self, row, col):
+        """Checks if the cell can be used as part of a new word in the crossword"""
+
+        # Assume that the cell is legal initially
+        cell_is_legal = True
+
+        # Only check neighbours if this cell is blank - adding a letter at the start
+        # or end of an existing word should be avoided, as it may not result in a new legal
+        # word. Running a new word through an existing word is fine, as it does not alter
+        # the existing word. In fact, it's the whole point of a crossword.
+        if self.grid(row, col) == "_":
+            has_cell_to_left = col > 0
+            has_cell_to_right = col < self.cols
+            if has_cell_to_left and self.grid[row][col - 1] != "_":
+                cell_is_legal = False
+            if has_cell_to_right and self.grid[row][col + 1] != "_":
+                cell_is_legal = False
+        
+        return cell_is_legal
+        
 
     def add_word_to_grid(self, word):
         """Adds a word to the crossword grid in the correct orientation"""
         for i in range(len(word.string)):
             if word.orientation == Orientation.HORIZONTAL:
-                self.grid[word.start_x + i][word.start_y] = word.string[i]
+                self.grid[word.start_y][word.start_x + i] = word.string[i]
             else:
-                self.grid[word.start_x][word.start_y + i] = word.string[i]
-
+                self.grid[word.start_y + i][word.start_x] = word.string[i]
 
 
     def print(self):
@@ -46,7 +95,6 @@ class Crossword:
         light_gray = Colors.get_background_color(220, 220, 220)
         dark_gray = Colors.get_background_color(0, 0, 0)
         text_color = Colors.get_foreground_color(0, 0, 0)
-        print()
         print("Here's the crossword : ")
         for row in self.grid:
             display_chars = []
@@ -54,7 +102,7 @@ class Crossword:
                 if char == '_':
                     display_chars.append(f"{dark_gray}{UniChars.EMPTY_SQUARE}{AnsiCommands.DEFAULT_COLOR}")
                 else:
-                    display_chars.append(f"{light_gray}{text_color}{get_large_letter(char)}")
+                    display_chars.append(f"{light_gray}{text_color}{get_large_letter(char)}{AnsiCommands.DEFAULT_COLOR}")
             string = ''.join(display_chars)
             print(string)
         
