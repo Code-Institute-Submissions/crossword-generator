86e2a324b1c03ff6e02769a23ebb1da787976902
John Rearden <reardenjohn@gmail.com>
2022-07-27 11:22:26 +0100

    Change frequency minimum in load_word_frequencies() funtion to 50

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     word_list = load_large_dictionary()
     print(f"Word_list loaded with {len(word_list)} entries")
     
-    frequency_list = load_word_frequencies()
+    frequency_dict = load_word_frequencies()
+    frequency_keys = frequency_dict.keys()
+    print(f"Frequency dict loaded with {len(frequency_keys)} entries")
     
     # Words in the word_list are often duplicated, with differing definitions.
     # Create a dictionary with words as keys, and definitions as a list of
@@ -15,23 +17,26 @@ def main():
     previous_word = ""
     total_count = 0
     unique_count = 0
+    not_in_freq_dict_count = 0
     for item in word_list:
-        item_as_list = item.split('|')
-        if len(item_as_list) > 2:
-            print("Something odd - more than 2 values separated by '|'")
-        word = item_as_list[0]
-        definition = item_as_list[1]
+        word = item[0]
+        definition = item[1]
         if word != previous_word:
             word_dict[word] = []
             word_dict[word].append(definition)
             unique_count += 1
             total_count += 1
+            if word not in frequency_keys:
+                not_in_freq_dict_count += 1
+                # print(f"{word} not in frequency_dict")
         else:
             word_dict[word].append(definition)
             total_count += 1
         previous_word = word
 
-    print(f"Dictionary created : {unique_count} unique, {total_count} total")
+    print(f"Dictionary created : {unique_count} unique, {total_count} total, {not_in_freq_dict_count} not in frequency dict")
+
+    # print(json.dumps(word_dict, indent=2))
     
 
 def compare_dict_against_frequencies():
@@ -58,26 +63,25 @@ def load_word_frequencies():
     """Loads the wikipedia word frequency file and reads all the entries that
        have a frequency of more than 500, then saves these entries to a new
        file."""
-    freq_tuple_list = []
+    freq_dict = {}
     with open('data/enwiki-20210820-words-frequency.txt', 'r') as infile:
         for line in infile:
             elements = line.split(' ')
             frequency = int(elements[1].replace('\n', ''))
-            if frequency >= 500:
-                freq_tuple = (elements[0], frequency)
-                freq_tuple_list.append(freq_tuple)
-            else: 
+            if frequency >= 50:
+                freq_dict[elements[0]] = frequency
+            else:
                 break
 
     try:
-        with open('data/wiki-words-freq500+.txt', 'x') as outfile:
-            for freq_tuple in freq_tuple_list:
-                outfile.writelines(f"{freq_tuple[0]} {freq_tuple[1]}\n")
+        with open('data/wiki-words-freq50+.txt', 'x') as outfile:
+            for key, value in freq_dict.items():
+                outfile.writelines(f"{key} {value}\n")
     except FileExistsError:
-        print("Skipping file creation - 'data/wiki-words-freq500+.txt' already exists")
+        print("Skipping file creation - 'data/wiki-words-freq50+.txt' already exists")
 
     
-    return freq_tuple_list
+    return freq_dict
 
 def load_large_dictionary():
     word_set = set()
@@ -96,7 +100,9 @@ def load_large_dictionary():
             # Reassemble all the values after the first 2, as many of
             # the definitions themselves contain commas!
             definition = ','.join(split_line[2:])
-
+            definition = definition.replace('"', '')
+            definition = definition.replace('\n', '')
+            
             # Ensure that the words contain the letters a-z only
             if word[0].isalpha() and '-' not in word[0]:
                 word_set.add((word.lower(), definition))
@@ -110,10 +116,8 @@ def load_large_dictionary():
                 write_file.writelines(f"{word[0]}|{word[1]}\n")
     except FileExistsError:
         print("Skipping file creation - 'data/large_dictionary.txt' already exists")
-    
-    
 
-    return word_list            
+    return word_list
 
 def find_matches(word, word_dict):
     known_chars = []
