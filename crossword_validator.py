75166f4ac8bc60141945c33387ff8b835e99243c
John Rearden <reardenjohn@gmail.com>
2022-08-12 20:58:15 +0100

    Ensure crossword_validator.py passes PEP8 validation

--- crossword_validator.py
+++ crossword_validator.py
@@ -3,15 +3,16 @@ import sys
 from constants import AnsiCommands, Orientation
 from crossword_generator import Crossword
 
+
 def main():
     """Main entry point for the program"""
     word_length_map = {}
     with open('data/crossword_dictionary.json', 'r', encoding='utf-8') as file:
         word_dict = json.load(file)
 
-        # Build a python dictionary with word lengths as keys, and lists of words of
-        # that length as values. The dictionary is used to search for matching
-        # partial words
+        # Build a python dictionary with word lengths as keys, and lists of
+        # words of that length as values. The dictionary is used to search for
+        # matching partial words
         for word in word_dict.keys():
             word = word.replace('\n', '')
             length = len(word)
@@ -26,12 +27,14 @@ def main():
         sys.stdout.write(AnsiCommands.CLEAR_BUFFER)
         sys.stdout.write(AnsiCommands.CLEAR_SCREEN)
         print(f"Testing number {counter}")
-        crossword = Crossword(12, 12, word_length_map, word_dict, user_present=True)
+        crossword = Crossword(12, 12, word_length_map, word_dict,
+                              user_present=True)
         result = validate(crossword)
         if result is False:
             print(f"problem at iteration {counter}")
             sys.exit()
     print(f"Tested {iterations} crosswords ... all valid")
+
 
 def validate(crossword):
     """Performs a suite of tests on a crossword, returning True if all
@@ -66,6 +69,7 @@ def validate(crossword):
 
     return True
 
+
 def check_for_2x2_groups(crossword):
     """Test ensures that no 2x2 group of contiguous cells are all occupied"""
     for i in range(crossword.rows - 1):
@@ -80,8 +84,9 @@ def check_for_2x2_groups(crossword):
                 return False
     return True
 
+
 def check_clue_strings_match_grid(crossword):
-    """Test ensures that all clue strings are represented accurately on the 
+    """Test ensures that all clue strings are represented accurately on the
        crossword grid. This would not be the case if a clue had overwritten
        the letters in another clue"""
     clues = crossword.clues_across + crossword.clues_down
@@ -92,8 +97,10 @@ def check_clue_strings_match_grid(crossword):
                 col = clue.start_col
                 grid_char = crossword.grid[row][col + i]
                 if char != grid_char:
-                    print(f"Characters do not match at row {row}, col {col + i} for {clue.string}")
-                    print(f"clue[{i}] == {clue.string[i]} , grid[{row}][{col + i}] == {grid_char}")
+                    print(f"Characters do not match at row {row}, "
+                          f"col {col + i} for {clue.string}")
+                    print(f"clue[{i}] == {clue.string[i]} , "
+                          f"grid[{row}][{col + i}] == {grid_char}")
                     return False
         elif clue.orientation == Orientation.VERTICAL:
             col = clue.start_col
@@ -101,10 +108,13 @@ def check_clue_strings_match_grid(crossword):
                 row = clue.start_row
                 grid_char = crossword.grid[row + i][col]
                 if char != grid_char:
-                    print(f"Characters do not match at row {row + i}, col {col} for {clue.string}")
-                    print(f"clue[{i}] == {clue.string[i]} , grid[{row + i}][{col}] == {grid_char}")
+                    print(f"Characters do not match at row {row + i}, "
+                          f"col {col} for {clue.string}")
+                    print(f"clue[{i}] == {clue.string[i]} , "
+                          f"grid[{row + i}][{col}] == {grid_char}")
                     return False
     return True
+
 
 def check_no_adjacent_clues(crossword):
     """Test ensures that no clue touches another without intersecting it"""
@@ -116,11 +126,13 @@ def check_no_adjacent_clues(crossword):
             length = len(clue.string)
             if clue.start_col > 0:
                 if crossword.grid[row][clue.start_col - 1] != '_':
-                    print(f"{clue.string} touches another clue at {row},{clue.start_col - 1}")
+                    print(f"{clue.string} touches another clue at {row},"
+                          f"{clue.start_col - 1}")
                     return False
             if clue.start_col + length < crossword.cols - 1:
                 if crossword.grid[row][clue.start_col + length] != '_':
-                    print(f"{clue.string} touches another clue at {row},{clue.start_col + length}")
+                    print(f"{clue.string} touches another clue at {row},"
+                          f"{clue.start_col + length}")
                     return False
         elif clue.orientation == Orientation.VERTICAL:
             # The cells bookending the clue must be blank
@@ -128,22 +140,28 @@ def check_no_adjacent_clues(crossword):
             length = len(clue.string)
             if clue.start_row > 0:
                 if crossword.grid[clue.start_row - 1][col] != '_':
-                    print(f"{clue.string} touches another clue at {clue.start_row - 1},{col}")
+                    print(f"{clue.string} touches another clue at "
+                          f"{clue.start_row - 1},{col}")
                     return False
             if clue.start_row + length < crossword.rows - 1:
                 if crossword.grid[clue.start_row + length][col] != '_':
-                    print(f"{clue.string} touches another clue at {clue.start_row + length},{col}")
+                    print(f"{clue.string} touches another clue at "
+                          f"{clue.start_row + length},{col}")
                     return False
     return True
 
+
 def check_all_clues_appear_in_dictionary(crossword):
-    """Test confirms that all clue strings do in fact appear in the dictionary"""
+    """Test confirms that all clue strings do in fact appear in the
+       dictionary"""
     clues = crossword.clues_across + crossword.clues_down
     for clue in clues:
         if clue.string not in crossword.word_dict:
-            print(f"The clue '{clue.string}' does not appear in the dictionary")
+            print(f"The clue '{clue.string}' does not "
+                  f"appear in the dictionary")
             return False
     return True
+
 
 if __name__ == '__main__':
     main()
